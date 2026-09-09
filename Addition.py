4a4a68576ce12fb7ad5b917c40e9993bdb738f52
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-09 11:48:26 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,18 @@
+a=10
+b=11
+
+c= a + b
+print("Addition is :",c)
+
+def Addition(a,b):
+    return a+b
+
+def main():
+    a=10
+    b=11
+
+    result=Addition(a,b)
+    print("Addition is :",result)
+
+if __name__=="__main__":
+    main()
