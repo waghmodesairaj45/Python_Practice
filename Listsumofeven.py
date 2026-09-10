e6825ed2bda15606ac25d1c8d3d9b744ce46df6d
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-10 21:18:44 +0530

    Add files via upload

---
+++ Listsumofeven.py
@@ -0,0 +1,7 @@
+num=[2,3,7,4,9,8]
+
+sum=0
+for i in num:
+    if (i%2==0):
+        sum+= i
+print(sum)
---
+++ listCount.py
@@ -0,0 +1,8 @@
+num=[5, 15, 8, 20, 12]
+
+count=0
+for i in num:
+    if(i>=10):
+        count +=1
+print(count)
+
