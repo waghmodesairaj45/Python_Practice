f31a6cad354641eda472ccf5d69a5d5806f10bdb
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-12 22:08:51 +0530

    Add files via upload

---
+++ DataDemo.py
@@ -0,0 +1,5 @@
+no=11
+
+print("Value Of no is :",no)
+print("ID of no is: ",id(no))
+print("Types of no is:",type(no))
---
+++ Sequence.py
@@ -0,0 +1,10 @@
+Data1=[10,20,30,40]                          #List
+Data2=(10,20,30,40)                          #Tuple
+Data3={10,20,30,40}                          #set
+Data4={"A":10,"B":20,"C":30,"D":40}          #dict
+Data5="Maharashtra"                          #str
+print(type(Data1))
+print(type(Data2))
+print(type(Data3))
+print(type(Data4))
+print(type(Data5))
