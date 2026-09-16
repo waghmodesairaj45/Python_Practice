d9597bfb61ef6b69378b0811e6bd32b25f443ef9
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-16 21:41:05 +0530

    Add files via upload

---
+++ Function3.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    print("Enter first number :")
+    value1=int(input())
+
+    print("Enter Second number :")
+    value2=int(input())
+
+    ret=Addition(value1,value2) # Error 
+
+    print("Addition is :",ret)
+
+if __name__=="__main__":
+    main()
