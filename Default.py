cf38e074043c6ba7fe15944b7e53a19e3f679e64
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-23 22:43:38 +0530

    Add files via upload

---
+++ Default.py
@@ -0,0 +1,13 @@
+def Area(Radius, PI=3.14):
+    Ans=PI*Radius*Radius
+    return Ans
+
+def main():
+    ret=Area(10.5)
+    print("Area of circle is : ",ret)
+
+    ret=Area(10.5,7.12)
+    print("Area of circle is : ",ret)
+
+if __name__=="__main__":
+    main()
