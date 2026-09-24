57a0dfb420c60879e8607f4acf2e71696dc236e6
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-24 18:36:02 +0530

    Add files via upload

---
+++ ListVSTuple.py
@@ -0,0 +1,18 @@
+#----------------------------------------------------
+#                       Lists             Tuple
+#----------------------------------------------------
+#ordered                yes                yes
+# Indexed               Yes                 Yes
+#Mutable                Yes                No
+#-----------------------------------------------------
+def main():
+    Data1=[10,20,30,40] #List
+    Data2=(10,20,30,40) #Tuple
+
+    print(Data1)
+    print(Data2)
+
+    print(Data1[0])
+    print(Data2[0])
+
+
---
+++ TupleDemo1.py
@@ -0,0 +1,14 @@
+def main():
+    Data=(10,20,30,40)
+
+    print(type(Data))
+    print(len(Data))
+
+    print(Data[0])
+    print(Data[1])
+    print(Data[2])
+    print(Data[3])
+
+    
+if __name__=="__main__":
+    main()
